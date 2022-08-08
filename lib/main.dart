fdff218ad07c5d5fbac6d2b3657e69092a913e39
jermelterzi <jv_ermel@hotmail.com>
2022-08-08 16:25:18 -0300

    Implementing pomodoro page and store to use MobX

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pomodoro/pages/pomodoro_page.dart';
+import 'package:pomodoro/store/pomodoro.store.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +12,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        Provider<PomodoroStore>(
+          create: (context) => PomodoroStore(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const PomodoroPage(),
       ),
-      home: const PomodoroPage(),
     );
   }
 }
--- lib/pages/pomodoro_page.dart
+++ lib/pages/pomodoro_page.dart
@@ -1,22 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:pomodoro/store/pomodoro.store.dart';
 import 'package:pomodoro/widgets/time_input.dart';
+import 'package:pomodoro/widgets/timer.dart';
+import 'package:provider/provider.dart';
 
 class PomodoroPage extends StatelessWidget {
   const PomodoroPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final store = Provider.of<PomodoroStore>(context);
+
     return Scaffold(
       body: Column(
-        children: const [
-          Text('Pomodoro'),
-          TimeInput(
-            title: 'Trabalho',
-            value: 25,
-          ),
-          TimeInput(
-            title: 'Descanso',
-            value: 5,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Expanded(child: Timer()),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40.0),
+            child: Observer(
+              builder: (_) => Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  TimeInput(
+                    title: 'Trabalho',
+                    value: store.workTime,
+                    incFunction: store.incrementWorkTime,
+                    decFunction: store.decrementWorkTime,
+                  ),
+                  TimeInput(
+                    title: 'Descanso',
+                    value: store.restTime,
+                    incFunction: store.incrementRestTime,
+                    decFunction: store.decrementRestTime,
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
